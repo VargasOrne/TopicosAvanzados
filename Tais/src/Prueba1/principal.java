44d73a542ac3d0a584c1e4cf6ad62d2e1a539533
Orne <Orne@EXO>
2018-08-23 13:47:39 -0300

    modificacion de impresion :P

--- Tais/src/Prueba1/principal.java
+++ Tais/src/Prueba1/principal.java
@@ -3,5 +3,6 @@ package Prueba1;
 public class principal{
 	public static void main(String[] arg ) {
 		System.out.print("----  Topicos Avanzados ----");
+		System.out.print("----  ................. ----");
 		}
 }
